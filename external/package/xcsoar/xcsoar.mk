48552d934c65736a7e560d47cc2709d784f28c3f
Sascha Ittner <sascha.ittner@modusoft.de>
2020-12-10 02:45:43 +0100

    moved to upstream xcsoar and add wayland patches

--- external/package/xcsoar/xcsoar.mk
+++ external/package/xcsoar/xcsoar.mk
@@ -4,19 +4,16 @@
 #
 ################################################################################
 
-XCSOAR_VERSION = v6.8.17-ovng1
-XCSOAR_SITE = https://github.com/sittner/XCSoar.git
+XCSOAR_VERSION = v6.8.17
+XCSOAR_SITE = https://github.com/XCSoar/XCSoar.git
 XCSOAR_SITE_METHOD = git
 XCSOAR_LICENSE = GPL-2.0
 XCSOAR_LICENSE_FILES = COPYING
 XCSOAR_DEPENDENCIES += zlib freetype libpng jpeg openssl libcurl librsvg imagemagick gettext dejavu boost sdl2
 
 XCSOAR_MAKE = $(TARGET_MAKE_ENV) $(MAKE) \
-	TARGET=UNIX \
-	ENABLE_SDL=y \
-	USE_SDL2=y \
-	USE_SDL2_BATTERY=n \
-	OPENGL=n \
+	TARGET=WAYLAND \
+	GLES=y \
 	FIXED_DATADIR=/xcsdata \
 	DESTDIR=$(TARGET_DIR) \
 	TCPREFIX=$(TARGET_CROSS) \
@@ -36,9 +33,9 @@ define XCSOAR_INSTALL_TARGET_CMDS
 	$(XCSOAR_MAKE) -C $(@D) install-bin install-mo
 endef
 
-define XCSOAR_INSTALL_INIT_SYSV
-	$(INSTALL) -D -m 755 $(BR2_EXTERNAL_OVNG_PATH)/package/xcsoar/S90xcsoar $(TARGET_DIR)/etc/init.d/S90xcsoar
-endef
+#define XCSOAR_INSTALL_INIT_SYSV
+#       $(INSTALL) -D -m 755 $(BR2_EXTERNAL_OVNG_PATH)/package/xcsoar/S90xcsoar $(TARGET_DIR)/etc/init.d/S90xcsoar
+#endef
 
 $(eval $(generic-package))
 
