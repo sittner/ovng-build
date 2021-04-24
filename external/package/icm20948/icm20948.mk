69912df14974c83c8ffb1dbc0edd7bc6d323299f
Sascha Ittner <sascha.ittner@modusoft.de>
2021-04-24 20:18:23 +0200

    updated icm20948 driver

--- external/package/icm20948/icm20948.mk
+++ external/package/icm20948/icm20948.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ICM20948_VERSION = f0c76d41384131aaa456c934f0fb1785ffaafc30
+ICM20948_VERSION = c3ea032790cb4007789044467ee91e20e0a05f78
 ICM20948_SITE = https://github.com/sittner/icm20948-mod.git
 ICM20948_SITE_METHOD = git
 ICM20948_LICENSE = GPL-2.0
